63176c18f808c200e0fb4e3bfaf6903e9e3d87ad
Andrei Dolnikov <adolnikov@embeddedalley.com>
2009-08-25 08:53:53 -0400

    Build libogg only if BUILD_WITH_GST option provided.
    Signed-off-by: Andrei Dolnikov <adolnikov@embeddedalley.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(strip $(BUILD_WITH_GST)),true)
+
 LOCAL_PATH := $(call my-dir)
 
 LIBOGG_TOP := $(LOCAL_PATH)
@@ -5,3 +7,5 @@ LIBOGG_TOP := $(LOCAL_PATH)
 include $(CLEAR_VARS)
 
 include $(LIBOGG_TOP)/src/Android.mk
+
+endif
